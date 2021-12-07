729635191012f004d40883800bf110102a549e5e
Alexander Gaivoronskiy <a.gvrnsk@gmail.com>
2021-12-07 16:54:11 +0200

    core/clock - tweaks

--- retro/clock.lua
+++ retro/clock.lua
@@ -7,30 +7,31 @@ Clock.rate = 30
 Clock.cpu = 333
 function Clock:init(params)
     Object.init(self, params)
-    self.on_start = params.on_start or function() return end
-    self.on_tick = params.on_tick or function() return end
-    self.on_loop = params.on_loop or function() return end
-    self.on_destroy = params.on_destroy or function() return end
-end
-function Clock:start(params)
     os.cpu(self.cpu)
     self.on_start = params.on_start or function() return end
     self.on_tick = params.on_tick or function() return end
     self.on_loop = params.on_loop or function() return end
     self.on_destroy = params.on_destroy or function() return end
-
-    self.on_start()
-    while true do
-        current_time = now()
-        min_delta = 1000 / self.rate;
-        self.delta = current_time - self.prev_tick
-        if (self.delta >= min_delta) then
-            self.prev_tick = current_time
-            self.on_tick(self.delta / min_delta)
+    self.loop_cr = coroutine.create(function()
+        while true do
+            current_time = now()
+            min_delta = 1000 / self.rate;
+            self.delta = current_time - self.prev_tick
+            if (self.delta >= min_delta) then
+                self.prev_tick = current_time
+                self.on_tick(self.delta / min_delta)
+            end
+            self.on_loop()
         end
-        self.on_loop()
-    end
-
+    end)
+end
+function Clock:start(params)
+    self:log("starting...")
+    self.on_start()
+    coroutine.resume(self.loop_cr)
+end
+function Clock:stop()
+    self.loop_cr.yield()
     self.on_destroy()
 end
 return Clock
